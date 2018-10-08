fdc593255eea39c7991dfb4f68cce6500371c236
Doan Minh Trung Nguyen <trungn1@student.unimelb.edu.au>
2018-10-09 02:19:39 +1100

    Shaders and textures

---
+++ Assets/Scripts/GroundScript.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GroundScript : MonoBehaviour
+{
+    public float speed = 5;
+    private float count =0;
+
+
+    // Called each frame
+    void Update()
+    {
+      transform.Translate(Vector3.back * Time.deltaTime*speed);
+      this.count +=Time.deltaTime*speed;
+      if (this.count >=10){
+        transform.Translate(Vector3.forward * this.count);
+        this.count = 0;
+      }
+    }
+
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,7 +12,7 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
     if (jump){
-      this.transform.Translate(Vector3.up * jumpspeed * (Time.deltaTime*5));
+      this.transform.Translate(Vector3.left * jumpspeed * (Time.deltaTime*5));
       jumpspeed=jumpspeed-(Time.deltaTime*5);
       if(jumpspeed<-2.0f){
         jump = false;
@@ -24,15 +24,15 @@ public class PlayerController : MonoBehaviour {
           jumpspeed = 2.0f;
 
       }
-	    if (Input.GetKeyDown(KeyCode.LeftArrow) && !jump)
+	    if (Input.GetKey(KeyCode.LeftArrow))
         {
-            this.transform.Translate(Vector3.left * speed * Time.deltaTime);
+            this.transform.Translate(Vector3.down * speed * Time.deltaTime);
         }
-        if (Input.GetKeyDown(KeyCode.RightArrow) && !jump)
+        if (Input.GetKey(KeyCode.RightArrow))
         {
-            this.transform.Translate(Vector3.right * speed * Time.deltaTime);
+            this.transform.Translate(Vector3.up * speed * Time.deltaTime);
         }
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && !jump)
         {
             GameObject projectile = Instantiate<GameObject>(projectilePrefab);
             projectile.transform.position = this.gameObject.transform.position;
