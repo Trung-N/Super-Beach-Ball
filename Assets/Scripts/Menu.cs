6c68717a44769eb52b7071e0dc9bb9ecbedc469a
Doan Minh Trung Nguyen <trungn1@student.unimelb.edu.au>
2018-10-19 02:36:58 +1100

    Change difficulties

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,16 +1,52 @@
-﻿using UnityEngine;
-﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour {
-
+	private int mode = 1;
+	public GameObject option;
+	public Text modeDisplay;
 
 	public void PlayGame () {
-	    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + mode);
+	}
 
-  public void QuitGame () {
-  	  Application.Quit();
-    }
+	public void QuitGame () {
+		Application.Quit();
+	}
+	public void Options () {
+		option.SetActive(true);
+		this.DisplayMode();
+	}
+	public void Back () {
+		option.SetActive(false);
+	}
+	public void ChangeDif () {
+		if(mode == 1){
+			this.SetHard();
+		}
+		else{
+			this.SetNormal();
+		}
+	}
+
+	private void DisplayMode(){
+		if(mode == 1){
+			modeDisplay.text = "Normal";
+		}
+		else{
+			modeDisplay.text = "Hard";
+		}
+	}
+
+	private void SetHard () {
+		mode = 2;
+		this.DisplayMode();
+	}
+	private void SetNormal () {
+		mode = 1;
+		this.DisplayMode();
+	}
 }
